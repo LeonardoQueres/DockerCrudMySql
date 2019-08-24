4f1c13cd9b955af895ce2abb82f1ebf22a9f7dbd
Leonardo Queres <leonardoqueres@hotmail.com>
2019-08-24 16:05:54 -0300

    Crud Mysql com docker

---
+++ WebApp/Context/ProdutoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Context
+{
+    public class ProdutoContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=faculdade;Uid=root;Pwd=leoq15/*;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public ProdutoContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
